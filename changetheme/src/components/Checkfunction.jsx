9eec1e39fc076e9ceed83ef7d3cd546c0204453d
Sarang Gunjal <sarang@clayfin.com>
2022-05-20 13:03:34 +0530

    ....

---
+++ changetheme/src/components/Checkfunction.jsx
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react'
+import { CartContext } from '../Context/CounterContext'
+import './Check.css'
+
+export default function Checkfunction() {
+    const {changedata,setChangedata} = useContext(CartContext)
+  return (
+
+
+    <div className='check'>
+        {changedata?
+            <div>this is check true</div>   : <h1>This is false</h1> 
+    }
+    </div>
+
+  )
+}
---
+++ changetheme/src/components/CounterContext.jsx
@@ -0,0 +1,13 @@
+import { createContext, useState } from "react";
+
+export const CartContext  = createContext();
+
+
+
+
+export const FunctionContext = ({children})=>{
+
+    const [changedata,setChangedata] = useState(true)
+    
+    return <CartContext.Provider value={{changedata,setChangedata}}>{children}</CartContext.Provider>
+}
